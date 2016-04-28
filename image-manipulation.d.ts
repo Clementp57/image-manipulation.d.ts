55edf6601b6ef2b1e1c1f5c60432460b61b7b409
Clementp57 <clementpeyrabere@me.com>
2016-04-28 11:17:21 +0200

    added constructor and instance methods

--- image-manipulation.d.ts
+++ image-manipulation.d.ts
@@ -1,6 +1,14 @@
 declare class ImageMethods {
+
+    constructor(canvas: HTMLCanvasElement);
+
+    crop(x: number, y: number, width: number, height: number): ImageMethods;
+    rotate(degrees: number): ImageMethods;
+    resize(width: number, height: number): ImageMethods;
+
     static getCanvasFromFile(file: File, callback: (canvas: HTMLCanvasElement) => any): void;
     static getCanvasFromImage(image: HTMLImageElement): HTMLCanvasElement;
     static getOrientationFromFile(file: File, callback: (canvas: HTMLCanvasElement) => any): void;
     static getCanvasFromUrl(url: string, callback: (canvas: HTMLCanvasElement) => any): void;
+
 }
