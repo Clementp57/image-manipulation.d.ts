e00eef83123aaee7317c7d1f3b1f76aa6a0eca86
Clementp57 <clementpeyrabere@me.com>
2016-04-28 11:11:14 +0200

    removed module

--- image-manipulation.d.ts
+++ image-manipulation.d.ts
@@ -1,9 +1,6 @@
-declare module imageManipulation {
-    export class ImageMethods {
-        static getCanvasFromFile(file: File, callback: (canvas: HTMLCanvasElement) => any): void;
-        static getCanvasFromImage(image: HTMLImageElement): HTMLCanvasElement;
-        static getOrientationFromFile(file: File, callback: (canvas: HTMLCanvasElement) => any): void;
-        static getCanvasFromUrl(url: string, callback: (canvas: HTMLCanvasElement) => any): void;
-        
-    }
+declare class ImageMethods {
+    static getCanvasFromFile(file: File, callback: (canvas: HTMLCanvasElement) => any): void;
+    static getCanvasFromImage(image: HTMLImageElement): HTMLCanvasElement;
+    static getOrientationFromFile(file: File, callback: (canvas: HTMLCanvasElement) => any): void;
+    static getCanvasFromUrl(url: string, callback: (canvas: HTMLCanvasElement) => any): void;
 }
