dda2228a7f074be4e5ba0112cff82392fe704d66
Clementp57 <clementpeyrabere@me.com>
2016-04-28 11:03:55 +0200

    first commit

---
+++ image-manipulation.d.ts
@@ -0,0 +1,9 @@
+declare module imageManipulation {
+    export class ImageMethods {
+        static getCanvasFromFile(file: File, callback: (canvas: HTMLCanvasElement) => any): void;
+        static getCanvasFromImage(image: HTMLImageElement): HTMLCanvasElement;
+        static getOrientationFromFile(file: File, callback: (canvas: HTMLCanvasElement) => any): void;
+        static getCanvasFromUrl(url: string, callback: (canvas: HTMLCanvasElement) => any): void;
+        
+    }
+}
